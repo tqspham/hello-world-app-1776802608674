88253e43ade5303daa7eace18d6a8d7b5bb4928e
tqspham <tqs.pham@gmail.com>
2026-04-21 22:17:08 +0200

    Adding app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-white">
+      <h1 className="text-5xl font-bold text-gray-900">Hello World</h1>
+    </div>
+  );
+}
